4f3100776a65f2367ddd48f20ee97f73bed6a6eb
Ryan Opel <ropel@newrelic.com>
2021-02-26 15:00:59 -0800

    2020-02-26 updates

--- install/zsh.sh
+++ install/zsh.sh
@@ -12,5 +12,9 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
+git clone https://github.com/lukechilds/zsh-nvm $ZSH_CUSTOM/plugins/zsh-nvm
+
+git clone https://github.com/mroth/evalcache $ZSH_CUSTOM/plugins/evalcache
+
 rm -rf ~/.zshrc
 
